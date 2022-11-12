0ff898419f615984424e566c0725ac68ef1927ab
Nishit Jha <jhanishitback@gmail.com>
2022-11-12 21:45:40 +0530

    Update and rename failed_midterms_4.sh to failed_science_midterms_4.sh

--- failed_science_midterms_4.sh
+++ failed_science_midterms_4.sh
@@ -1,10 +1,11 @@
 failed=0
 while IFS=$'\t', read -r id fname lname class section stream midterm_marks final_marks bus_route phone_number email_address; do
     
-
-        if [[ $midterm_marks < 33 ]]; then
-            failed=$((failed+1))
-        fi
+    if [[ $stream = "science" ]]; then
+            if [[ $midterm_marks < 33 ]]; then
+                failed=$((failed+1))
+            fi
+    fi
 
 done < data.tsv
 
