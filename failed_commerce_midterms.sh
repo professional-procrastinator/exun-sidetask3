5642ebe47e37cd042a8850e7be614c93618ad16e
Nishit Jha <jhanishitback@gmail.com>
2022-11-12 21:47:02 +0530

    Create failed_commerce_midterms.sh

---
+++ failed_commerce_midterms.sh
@@ -0,0 +1,12 @@
+failed=0
+while IFS=$'\t', read -r id fname lname class section stream midterm_marks final_marks bus_route phone_number email_address; do
+    
+    if [[ $stream = "commerce" ]]; then
+            if [[ $final_marks < 33 ]]; then
+                failed=$((failed+1))
+            fi
+    fi
+
+done < data.tsv
+
+echo $failed
